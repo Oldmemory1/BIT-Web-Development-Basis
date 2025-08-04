e2bb3ad85dd136d6cc8a3faa7d14da817ea36ecc
OldMemory1 <3558066700@qq.com>
2025-08-04 15:53:38 +0800

    init

---
+++ src/main/java/org/example/webproject1/Controller/Main_Controller.java
@@ -0,0 +1,8 @@
+package org.example.webproject1.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Main_Controller {
+
+}
---
+++ src/test/java/org/example/webproject1/WebProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
