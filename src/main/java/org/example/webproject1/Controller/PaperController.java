c19add82d6f5238005a03798939ec8d4230506fe
OldMemory1 <3558066700@qq.com>
2025-08-05 09:39:32 +0800

    modify PaperController.java

--- src/main/java/org/example/webproject1/Controller/PaperController.java
+++ src/main/java/org/example/webproject1/Controller/PaperController.java
@@ -3,7 +3,6 @@ package org.example.webproject1.Controller;
 import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,7 @@ import java.util.*;
 @Controller
 public class PaperController {
     @RequestMapping("/paper")
-    public String openPaper(Model model){
+    public String openPaper(){
         return "paper";
     }
     @PostMapping("/api/submit_answer")
