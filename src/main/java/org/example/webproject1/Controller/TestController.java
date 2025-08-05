ce1ae2043a49c684f1a606bb345f7fcc2eec018d
OldMemory1 <3558066700@qq.com>
2025-08-05 09:58:36 +0800

    modify TestController.java temp_page.html

--- src/main/java/org/example/webproject1/Controller/TestController.java
+++ src/main/java/org/example/webproject1/Controller/TestController.java
@@ -1,12 +1,24 @@
 package org.example.webproject1.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
 
 @Controller
 public class TestController {
     @RequestMapping("/test")
     public String test(){
         return "temp_page";
+    }
+    @GetMapping("/api/test")
+    public ResponseEntity<?> getContent(){
+        return ResponseEntity.ok(Map.of(
+                "status", "success",
+                "message", "测试信息",
+                "data", "内容1"
+        ));
     }
 }
